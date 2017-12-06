f4f4f607a4faf785743420e905be743ed73a1574
Felipe Coury <felipe.coury@gmail.com>
2017-12-06 17:42:35 -0200

    feat: allow using a transform file
    Fixes #1

--- src/log.js
+++ src/log.js
@@ -1,9 +1,50 @@
 const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const ini = require('ini');
 const _ = require('lodash');
+
+let _transform = 'unloaded';
+
+function loadTransform(_fs=fs) {
+  if (_transform === 'unloaded') {
+    const transformFile = path.join(os.homedir(), '.json-log-viewer');
+    if (!_fs.existsSync(transformFile)) {
+      return;
+    }
+
+    const contents = _fs.readFileSync(transformFile, 'utf8');
+    const { transform } = ini.parse(contents);
+    if (!transform) {
+      return;
+    }
+
+    _transform = transform;
+  }
+
+  return _transform;
+}
+
+function transform(entry, _fs=fs) {
+  const transform = loadTransform(_fs);
+  if (!transform) {
+    return entry;
+  }
+
+  return Object.keys(transform).reduce((hash, key) => {
+    const value = transform[key];
+    if (value === '$') {
+      hash[key] = _.cloneDeep(entry);
+    } else {
+      hash[key] = _.get(entry, value);
+    }
+    return hash;
+  }, {});
+}
 
 function parse(line) {
   try {
-    return JSON.parse(line);
+    return transform(JSON.parse(line));
   } catch (e) {
     return null;
   }
@@ -20,4 +61,4 @@ function readLog(file, reader=fs) {
   });
 };
 
-module.exports = { readLog };
+module.exports = { readLog, transform };
